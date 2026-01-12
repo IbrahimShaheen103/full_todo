5d40dc22e7bffc5d8e314db1b0f263aec387b691
Ibrahim <ishaheen910@gmail.com>
2026-01-12 09:17:52 +0200

    `Update app/_layout.tsx to include two Stack.Screen components`

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,23 @@
 import { Stack } from "expo-router";
 
 export default function Layout() {
-  return <Stack screenOptions={{ headerShown: false }} />;
+  return (
+    <Stack>
+      <Stack.Screen
+        name="index"
+        options={{
+          headerShown: false,
+        }}
+      />
+      <Stack.Screen
+        name="details"
+        options={{
+          presentation: "formSheet",
+          sheetAllowedDetents: [0.5, 0.7, 1],
+          sheetCornerRadius: 20,
+          sheetGrabberVisible: true,
+        }}
+      />
+    </Stack>
+  );
 }
